53b293724b2f0e8795cf31e7e565c8ec2b6d5263
sneelyg <santiagoneely@gmail.com>
2022-07-23 01:18:28 +0000

    fin de clase

--- exercises/06-Create-DOM-Element-Second/index.js
+++ exercises/06-Create-DOM-Element-Second/index.js
@@ -1,3 +1,5 @@
 //you code here
 document.body.innerHTML = "<h1>Hello World</h1>";
-
+document.body.innerHTML = "<img src='https://via.placeholder.com/350x150' alt='google advertising' /> ";
+document.body.innerHTML = "<img src='https://via.placeholder.com/350x150' alt='google advertising' /> ";
+document.body.innerHTML = "<img src='https://via.placeholder.com/350x150' alt='google advertising' /> ";
--- exercises/08.1-Remove-DOM-Element/index.js
+++ exercises/08.1-Remove-DOM-Element/index.js
@@ -1,3 +1,10 @@
 // your code here
 // if you check the HTML, you will find that the second LI has the id=secondElement
 // you can use that to your advantage as a CSS selector
+
+
+//var aux = document.querySelector("ul");
+//aux.parentNode.removeChild("#secondElement");
+
+let aux2 = document.querySelector("#secondElement");
+aux2.parentNode.removeChild(aux2);
